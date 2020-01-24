870c5064c6019c3aaefa6d574da41ec3fa56bac2
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-01-24 13:04:58 -0500

    almost done with answers

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,14 @@
 
 ## Exercise I
 
-a)
+a) The time complexity is O(n) because the number of times that the loop will run is proportional to n. 
 
 
 b)
 
 
-c)
+c) The time complexity is O(n) because the function will get called approximately n times. 
 
 ## Exercise II
 
-
+One solution is to drop eggs starting in the first floor and keep going upstairs until the egg breaks. This would be very inefficient. The best solution would be to start at n/2 and see if the egg breaks. If it breaks, try n/4 and so on. This is like a binary search. The time complexity is O(log(n))
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,16 +3,15 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-count = 0
 
-def count_th(word):    
-    global count
+
+def count_th(word, count = 0):    
     if len(word) == 0:
         return count
     elif len(word) > 0:
         if word[0:2] == 'th':
             count += 1           
-        return count_th(word[1:]) 
+        return count_th(word[1:], count) 
 
 
 
